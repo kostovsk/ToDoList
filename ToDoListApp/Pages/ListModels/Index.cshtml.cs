a4b9fb29a22eb45c70bef5a4f83fa62776b73039
Stoycho Kostov <stoichokostov@yahoo.com>
2020-12-01 11:10:43 -0600

    azure db

--- ToDoListApp/Pages/ListModels/Index.cshtml.cs
+++ ToDoListApp/Pages/ListModels/Index.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDoListApp.Data;
+using ToDoListApp.Models;
 
 namespace ToDoListApp.Pages.ListModels
 {
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+   public class IndexModel : PageModel
+   {
+      private readonly ApplicationDbContext _db;
+
+      public IndexModel(ApplicationDbContext db)
+      {
+         _db = db;
+      }
+
+      public IList<ListModel> ListModel { get; set; }
+
+      public async Task<IActionResult> OnGet()
+      {
+         ListModel = await _db.ListModel.ToListAsync();
+         return Page();
+      }
+   }
 }
