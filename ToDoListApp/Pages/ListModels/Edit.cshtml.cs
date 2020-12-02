59039972463bf01bc9bcc1ff3f0df1f18ce4d8d4
Michael Kostov <kostovmichael@yahoo.com>
2020-12-01 18:33:49 -0700

    sandbox and edit page

---
+++ ToDoListApp/Pages/ListModels/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDoListApp.Data;
+using ToDoListApp.Models;
+
+namespace ToDoListApp.Pages.ListModels
+{
+    public class EditModel : PageModel
+    {
+      private readonly ApplicationDbContext _db;
+
+      public EditModel(ApplicationDbContext db)
+      {
+         _db = db;
+      }
+
+      public ListModel TheCurrentList { get; set; }
+
+     
+
+      public async Task<IActionResult> OnGet()
+      {
+
+         //gets the id from the Query String: https://localhost:44306/ListModels/Edit?Id=1
+
+         string idFromQueryString = Request.Query["Id"];
+         /*
+          TODO: 
+            check for null, if null then redirect back to Index page with the ItemsList, 
+            else convert 'idFromQueryString' to int and use the _db to find the item
+          */
+
+         return Page();
+      }
+   }
+}
---
+++ ToDoListApp/Pages/ListModels/Sandbox.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+
+namespace ToDoListApp.Pages.ListModels
+{
+   public class Sandbox : PageModel
+   {
+
+
+
+      public void OnGet()
+      {
+
+      }
+   }
+}
