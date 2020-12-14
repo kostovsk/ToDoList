65448f50b3ad1c58a68093bb2784c795d6e085f7
Stoycho Kostov <stoichokostov@yahoo.com>
2020-12-14 17:46:11 -0600

    requiredfieldsupdate

--- ToDoListApp/Models/ToDoList.cs
+++ ToDoListApp/Models/ToDoList.cs
@@ -3,13 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ToDoListApp.Models
 {
-    public class ToDoList
-    {
-        
-        [Key]
-        public int LIST_ID { get; set; }
-  
-        public string NAME { get; set; }
-        public DateTime DATE_CREATED { get; set; }
-    }
+   public class ToDoList
+   {
+
+      [Key]
+      public int LIST_ID { get; set; }
+      [Required]
+      public string NAME { get; set; }
+      public DateTime DATE_CREATED { get; set; }
+   }
 }
